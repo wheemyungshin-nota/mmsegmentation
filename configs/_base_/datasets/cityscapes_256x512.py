7297a75c48ad28559243a9e6fbc451494a6ff962
wheemyungshin <zerger26@gmail.com>
2023-03-24 19:33:05 +0900

    add cfg

--- configs/_base_/datasets/cityscapes_256x512.py
+++ configs/_base_/datasets/cityscapes_256x512.py
@@ -3,11 +3,11 @@ dataset_type = 'CityscapesDataset'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (256, 512)
+crop_size = (128, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(512, 1024), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(512, 256), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(512, 1024),
+        img_scale=(512, 256),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
